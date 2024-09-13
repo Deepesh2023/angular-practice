ad3bb8acc640c723daa269a8066cfc1540348bc1
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-09-13 09:12:55 +0530

    HousingLocation interface

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [],
   templateUrl: './housing-location.component.html',
-  styleUrl: './housing-location.component.css'
+  styleUrl: './housing-location.component.css',
 })
-export class HousingLocationComponent {
-
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
