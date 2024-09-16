0885b7c799b0802a6452eecf277fefbe81a254b6
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-09-16 09:47:26 +0530

    basic routing config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
-  templateUrl: "./app.component.html",
+  imports: [HomeComponent, RouterModule],
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
