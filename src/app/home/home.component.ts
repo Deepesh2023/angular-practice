a8fbffb4b8ad09a3ff30cb200ba0ffe808a1c72c
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-09-17 10:52:25 +0530

    added search functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,19 +3,34 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
 import { HousingLocation } from '../housinglocation';
 import { HousingService } from '../housing.service';
 import { NgFor } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousingLocationComponent, NgFor],
+  imports: [HousingLocationComponent, NgFor, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  housingLocationList: HousingLocation[] | null = null;
+  housingLocationList: HousingLocation[] | undefined;
+  filteredHousingLocationList: HousingLocation[] | undefined;
+
   housingService: HousingService = inject(HousingService);
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredHousingLocationList = this.housingLocationList;
+  }
+
+  filterResults(filter: string) {
+    if (!filter) {
+      this.filteredHousingLocationList = this.housingLocationList;
+    }
+
+    this.filteredHousingLocationList = this.filteredHousingLocationList?.filter(
+      (housingLocation) =>
+        housingLocation.city.toLowerCase().includes(filter?.toLowerCase())
+    );
   }
 }
