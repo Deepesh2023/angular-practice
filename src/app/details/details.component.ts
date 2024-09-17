dcae36e5e96b56de816425fc2cd78f0fe87d4b97
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-09-17 09:08:16 +0530

    added basic form input

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
 import { HousingService } from '../housing.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css',
 })
@@ -16,9 +18,23 @@ export class DetailsComponent {
 
   housingLocation: HousingLocation | undefined;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation =
       this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
